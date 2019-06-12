8ec0404085c4c3bbc89d62a3f4477b3961f74f25
PDFK <pedrof_42@yahoo.es>
2019-06-12 17:46:47 -0400

    [preview question]

--- src/components/Questions/killer_condition_select.jsx
+++ src/components/Questions/killer_condition_select.jsx
@@ -1,96 +1,171 @@
-import React from 'react';
-import Select from 'react-select'
-import InputError from './error.jsx';
+import React from "react";
+import Select from "react-select";
+import InputError from "./error.jsx";
 
 class KillerConditionSelect extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      killer_condition: props.question.killer_condition || "",
+      killer_condition: props.question.killer_condition || ""
     };
     this.handleKillerValue = this.handleKillerValue.bind(this);
   }
 
   validOptions(selected_option) {
-    const {t} = this.props;
+    const { t } = this.props;
     const killer_condition_types = [
-      { value: "<", label: t("questions.select_options.killer_condition_types.smaller_than") },
-      { value: "<=", label: t("questions.select_options.killer_condition_types.less_than_or_equal_to") },
-      { value: "==", label: t("questions.select_options.killer_condition_types.equal") },
-      { value: "!=", label: t("questions.select_options.killer_condition_types.distinct") },
-      { value: ">", label: t("questions.select_options.killer_condition_types.greater_than") },
-      { value: ">=", label: t("questions.select_options.killer_condition_types.greater_than_or_equal") },
-      { value: "between?", label: t("questions.select_options.killer_condition_types.between") },
-      { value: "not_between?", label: t("questions.select_options.killer_condition_types.not_between") },
-      { value: "is_option?", label: t("questions.select_options.killer_condition_types.is_option") },
-      { value: "isnt_option?", label: t("questions.select_options.killer_condition_types.isnt_option") },
-      { value: "are_options?", label: t("questions.select_options.killer_condition_types.are_options") },
-      { value: "arent_options?", label: t("questions.select_options.killer_condition_types.arent_options") },
-      { value: "some_option?", label: t("questions.select_options.killer_condition_types.some_option") },
-      { value: "none_choice?", label: t("questions.select_options.killer_condition_types.none_choice") }
+      {
+        value: "<",
+        label: t("questions.select_options.killer_condition_types.smaller_than")
+      },
+      {
+        value: "<=",
+        label: t(
+          "questions.select_options.killer_condition_types.less_than_or_equal_to"
+        )
+      },
+      {
+        value: "==",
+        label: t("questions.select_options.killer_condition_types.equal")
+      },
+      {
+        value: "!=",
+        label: t("questions.select_options.killer_condition_types.distinct")
+      },
+      {
+        value: ">",
+        label: t("questions.select_options.killer_condition_types.greater_than")
+      },
+      {
+        value: ">=",
+        label: t(
+          "questions.select_options.killer_condition_types.greater_than_or_equal"
+        )
+      },
+      {
+        value: "between?",
+        label: t("questions.select_options.killer_condition_types.between")
+      },
+      {
+        value: "not_between?",
+        label: t("questions.select_options.killer_condition_types.not_between")
+      },
+      {
+        value: "is_option?",
+        label: t("questions.select_options.killer_condition_types.is_option")
+      },
+      {
+        value: "isnt_option?",
+        label: t("questions.select_options.killer_condition_types.isnt_option")
+      },
+      {
+        value: "are_options?",
+        label: t("questions.select_options.killer_condition_types.are_options")
+      },
+      {
+        value: "arent_options?",
+        label: t(
+          "questions.select_options.killer_condition_types.arent_options"
+        )
+      },
+      {
+        value: "some_option?",
+        label: t("questions.select_options.killer_condition_types.some_option")
+      },
+      {
+        value: "none_choice?",
+        label: t("questions.select_options.killer_condition_types.none_choice")
+      }
     ];
     var options = [];
 
-    switch(selected_option) {
+    switch (selected_option) {
       case "currency":
       case "date":
       case "int":
-          options = ["<", "<=", "==", "!=", ">", ">="];
-          break;
+        options = ["<", "<=", "==", "!=", ">", ">="];
+        break;
       case "boolean":
       case "string":
-          options = ["==", "!="];
-          break;
+        options = ["==", "!="];
+        break;
       case "range":
-          options = ["between?", "not_between?"];
-          break;
+        options = ["between?", "not_between?"];
+        break;
       case "option":
-          options = ["is_option?", "isnt_option?"];
-          break;
+        options = ["is_option?", "isnt_option?"];
+        break;
       case "multiple_option":
-          options = ["are_options?", "arent_options?", "some_option?", "none_choice?"];
-          break;
+        options = [
+          "are_options?",
+          "arent_options?",
+          "some_option?",
+          "none_choice?"
+        ];
+        break;
       default:
-          options = []
+        options = [];
     }
 
-    var valid_options = killer_condition_types.filter(function(item){
-      return options.includes(item.value);
-    }) || [];
+    var valid_options =
+      killer_condition_types.filter(function(item) {
+        return options.includes(item.value);
+      }) || [];
 
     return valid_options;
   }
 
   drawKillerCondition() {
-
     const killer_condition = this.state.killer_condition;
     const options = this.validOptions(this.props.selected_option);
     const name = this.props.name;
-    let value = options.find(function(item){
-      return item.value == killer_condition;
-    }) || [];
+    let value =
+      options.find(function(item) {
+        return item.value == killer_condition;
+      }) || [];
 
     return (
-      <Select className="" onChange={ this.handleKillerValue } options={options} value={ value } name={ `${name}[killer_condition]` } />
-    )
+      <Select
+        className=""
+        onChange={this.handleKillerValue}
+        options={options}
+        value={value}
+        name={`${name}[killer_condition]`}
+      />
+    );
   }
 
   handleKillerValue(selectedOption) {
     var option = selectedOption.value;
-    this.setState({killer_condition: option});
+    this.props.handleChangeStatus("killer_condition", option);
+    this.setState({ killer_condition: option });
+  }
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.killer_condition !== prevState.killer_condition) {
+      return { killer_condition: nextProps.killer_condition };
+    } else return null;
   }
 
   render() {
     const { t } = this.props;
-    const label = this.props.selected_option == "multiple_option" ? "killer_condition_multiple" : "killer_condition";
+    const label =
+      this.props.selected_option == "multiple_option"
+        ? "killer_condition_multiple"
+        : "killer_condition";
     return (
-      <div className="form-group col-sm-3">
-        <label htmlFor="" className="label-bold">{ t(`questions.attributes.${label}`) }</label>
-        { this.drawKillerCondition() }
-        <InputError attr="killer_condition" errors={ this.props.question.errors } />
+      <div className="form-group col-sm-6">
+        <label htmlFor="" className="label-bold">
+          {t(`questions.attributes.${label}`)}
+        </label>
+        {this.drawKillerCondition()}
+        <InputError
+          attr="killer_condition"
+          errors={this.props.question.errors}
+        />
       </div>
     );
   }
-};
+}
 
 export default KillerConditionSelect;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,62 @@
-import React, { Component } from 'react'
-import Questions from './components/Questions/index.jsx'
-import { I18nextProvider } from 'react-i18next';
-import i18n from './i18n/index.js';
+import React, { Component } from "react";
+import Questions from "./components/Questions/index.jsx";
+import { I18nextProvider } from "react-i18next";
+import i18n from "./i18n/index.js";
+import ReactDOM from "react-dom";
 
 export default class MultipleQuestions extends Component {
-
   render() {
+    const questions = [
+      {
+        killer_value: "1;3",
+        killer_condition: "are_options?",
+        description: "text",
+        value_type: "multiple_option",
+        order: "0",
+        options: ["1", "2", "3", "4"]
+      },
+      {
+        killer_value: ["1", "3"],
+        killer_condition: "isnt_option?",
+        description: "text",
+        value_type: "option",
+        order: "2",
+        options: ["1", "2", "3", "4"]
+      },
+      {
+        killer_value: ["1"],
+        killer_condition: "isnt_option?",
+        description: "text",
+        value_type: "option",
+        order: "3",
+        options: ["1", "2", "3", "4"]
+      },
+      {
+        killer_value: "4",
+        killer_condition: "isnt_option?",
+        description: "text",
+        value_type: "option",
+        order: "4",
+        options: ["1", "2", "3", "4"]
+      },
+      {
+        killer_value: "Si",
+        killer_condition: "==",
+        description: "text",
+        value_type: "boolean",
+        order: "5"
+      }
+    ];
+
     return (
       <I18nextProvider i18n={i18n}>
-        <Questions questions={ this.props.questions || [] } form_name={ this.props.form_name } />
-       </I18nextProvider>
-    )
+        <Questions
+          questions={this.props.questions || questions}
+          form_name={this.props.form_name}
+        />
+      </I18nextProvider>
+    );
   }
 }
 
+ReactDOM.render(<MultipleQuestions />, document.getElementById("root"));
