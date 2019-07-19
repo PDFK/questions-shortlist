3c8952d2d45298ce7598961ecf7daffd3545c554
Pedro Salazar <pedrof_42@yahoo.es>
2019-07-19 15:21:57 -0400

    [Cambios para subir componente]

--- src/index.js
+++ src/index.js
@@ -6,52 +6,11 @@ import ReactDOM from "react-dom";
 
 export default class MultipleQuestions extends Component {
   render() {
-    const questions = [
-      {
-        killer_value: "1;3",
-        killer_condition: "are_options?",
-        description: "text",
-        value_type: "multiple_option",
-        order: "0",
-        options: ["1", "2", "3", "4"]
-      },
-      {
-        killer_value: ["1", "3"],
-        killer_condition: "isnt_option?",
-        description: "text",
-        value_type: "option",
-        order: "2",
-        options: ["1", "2", "3", "4"]
-      },
-      {
-        killer_value: ["1"],
-        killer_condition: "isnt_option?",
-        description: "text",
-        value_type: "option",
-        order: "3",
-        options: ["1", "2", "3", "4"]
-      },
-      {
-        killer_value: "4",
-        killer_condition: "isnt_option?",
-        description: "text",
-        value_type: "option",
-        order: "4",
-        options: ["1", "2", "3", "4"]
-      },
-      {
-        killer_value: "Si",
-        killer_condition: "==",
-        description: "text",
-        value_type: "boolean",
-        order: "5"
-      }
-    ];
 
     return (
       <I18nextProvider i18n={i18n}>
         <Questions
-          questions={this.props.questions || questions}
+          questions={this.props.questions || []}
           form_name={this.props.form_name}
         />
       </I18nextProvider>
